12d45ad3f7d2d26bc11a0ca6c1610a94fc30cf11
mohammed-sada <mohammedabusada94@example.com>
2021-03-14 12:54:40 +0200

    changes

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react"
 import resumeFile from "../assets/res.pdf"
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
 
 const resume = () => {
-    return (
-        <div>
-            <embed src={resumeFile} type="application/pdf" width="100%" className="resume"/>
-        </div>
-    )
+  return (
+    <Layout>
+      <SEO title="Resume" description="webdev resume" />
+      <embed
+        src={resumeFile}
+        type="application/pdf"
+        width="100%"
+        className="resume"
+      />
+    </Layout>
+  )
 }
 
 export default resume

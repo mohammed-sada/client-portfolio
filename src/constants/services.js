c11ac610919545005c30b84e74c1e12992ff50a0
pydev2018 <abhishake88@gmail.com>
2021-03-16 14:14:12 +0400

    added the areas of interest

--- src/constants/services.js
+++ src/constants/services.js
@@ -4,43 +4,45 @@ import { GiProcessor } from "react-icons/gi"
 import { AiOutlineCloud ,AiOutlineRadarChart} from "react-icons/ai"
 import { FcStatistics } from "react-icons/fc"
 import { GrDeploy } from "react-icons/gr"
-import { FaPython } from "react-icons/fa"
+//import { FaPython } from "react-icons/fa"
+import { FaDungeon } from "react-icons/fa"
+
 
 export default [
   {
     id: 1,
     icon: <GiProcessor className="service-icon" />,
-    title: "Machine Learning",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "Data Mining and Statistical Analysis",
+    text: `Doing exploratory data analysis and predictive models to reveal patterns and trends in data and translate business problem to a data question, finding the best story to answer that question.`,
   },
   {
     id: 2,
     icon: <AiOutlineCloud className="service-icon" />,
-    title: "Cloud Compute",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "Cloud and Distributed Computing",
+    text: `Designing and implementing enterprise infrastructure and platforms required for cloud and distributed computing, analyze system requirements and ensure smooth integration or migration.`,
   },
   {
     id: 3,
-    icon: <FaPython className="service-icon" />,
-    title: "Deep learning",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    icon: <FaDungeon className="service-icon" />,
+    title:  "ML / Cognitive Computing Development",
+    text: `Getting inputs, building data pipelines, convenient data sources, A/B testing and bench marking infrastructure.`,
    },
   {
     id: 4,
     icon: <FcStatistics className="service-icon" />,
-    title: "Statistics",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "Business Intelligence and Strategy",
+    text: `improving back-end data sources for increased accuracy and simplicity, building tailored analytics solutions, managing dashboards, reporting to stakeholders, recognizing best practices in reporting and analysis: data integrity, test design, analysis, validation, and documentation.`,
   },
   {
     id: 5,
     icon: <GrDeploy className="service-icon" />,
-    title: "Model Deployment",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "Operations-Related Data Analytics",
+    text: `leveraging the tools and data provided by the other members of the data science team in order to find opportunities of improvement within the operations of the business.`,
   },
   {
     id: 6,
     icon: <AiOutlineRadarChart className="service-icon" />,
-    title: " Data visualization",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "Data Visualization and Presentation",
+    text: `Present data in a visually appealing way creating BI solutions for teams and customers based on specific business requirements and use cases`,
   },
 ]

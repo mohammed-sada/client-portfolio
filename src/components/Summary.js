7cdcda8f3592d44a5994b86c955f16ff58c0784a
pydev2018 <abhishake88@gmail.com>
2021-03-31 12:12:38 +0400

    changed the summary

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -7,7 +7,7 @@ const Summary = () => {
       <Title title="summary" />
       <div className="summary-center">
         <p>
-          Growth-focused thought leader with a nine-year achievement record encompassing all facets of data processing, analytics, and reporting. Committed
+          Growth-focused Data scientist and geophysicist with a nine-year achievement record encompassing all facets of data processing, analytics, and reporting. Committed
           to continuous innovation, has a verifiable record of conceptualizing and delivering innovative data and analytics solutions that provide organizations
           with critical business insights and facilitate sustainable business success. Adept at directing cross-functional teams in completing complex projects,
           delivering impressive results, and exceeding all stakeholder expectations.
